50ed46313b5fbdf3f907fd0c598009302601f52c
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-11-11 20:09:42 +0100

    add dynamic content and ejs templates

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs'); // templates engine to be uses
+app.set('views', 'views'); // where to find dynamics views; /views is default
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // static served files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
+    res.render('404', {pageTitle: 'Page Not Found', path: null});
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path')
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir ,'views', 'shop.html'));
+    // e pass the data to be used in the template as an object with an id key
+    res.render('shop', {prods: adminData.products, pageTitle: 'My Shop', path: '/'}); // use the default templates engine and return It
 });
 
 module.exports = router;
