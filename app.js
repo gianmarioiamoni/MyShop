04e200d41a5d78eb3843198c98e11b55c37a4e86
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-11-17 02:02:42 +0100

    add login form

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
